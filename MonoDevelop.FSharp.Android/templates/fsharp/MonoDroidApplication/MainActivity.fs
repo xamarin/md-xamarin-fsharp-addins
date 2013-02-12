5ac7669ad205d5655e172b632cb9b6860ae29ce5
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2013-02-12 18:02:22 +0900

    initial checkin.

---
+++ MonoDevelop.FSharp.Android/templates/fsharp/MonoDroidApplication/MainActivity.fs
@@ -0,0 +1,33 @@
+﻿namespace ${Namespace}
+
+module MainActivity =
+
+  open System
+
+  open Android.App
+  open Android.Content
+  open Android.OS
+  open Android.Runtime
+  open Android.Views
+  open Android.Widget
+
+  [<Activity (Label = "${ProjectName}", MainLauncher = true)>]
+  type MainActivity () =
+    inherit Activity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate(bundle:Bundle) =
+
+      base.OnCreate (bundle)
+
+      // Set our view from the "main" layout resource
+      this.SetContentView (Resource_Layout.Main)
+
+      // Get our button from the layout resource,
+      // and attach an event to it
+      let button = this.FindViewById<Button> Resource_Id.myButton
+      let onClick (args:EventArgs) =
+        button.Text <- String.Format ("{0} clicks!", count)
+        count <- count + 1
+      button.Click.Add (onClick)
