c9a064e311a7a3a2e7afc45d81c485e14977047d
Jeffrey Stedfast <jeff@xamarin.com>
2013-03-11 15:42:21 -0400

    Added initial iOS F# addin project

--- XamarinStudio.FSharp.MonoMac/Properties/AssemblyInfo.cs
+++ XamarinStudio.FSharp.MonoMac/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("XamarinStudio.FSharp.MonoMac")]
+[assembly: AssemblyTitle("XamarinStudio.FSharp.Mac")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xamarin Inc.")]
