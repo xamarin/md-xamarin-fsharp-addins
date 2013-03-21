2024e1dac52ac40f818861a1069f02cf98a14c54
Jeffrey Stedfast <jeff@xamarin.com>
2013-03-21 12:29:59 -0400

    Step 2 in renaming XamarinStudio back to MonoDevelop

--- MonoDevelop.FSharp.Android/Properties/AssemblyInfo.cs
+++ MonoDevelop.FSharp.Android/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("XamarinStudio.FSharp.Android")]
+[assembly: AssemblyTitle("MonoDevelop.FSharp.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xamarin Inc.")]
--- MonoDevelop.FSharp.iOS/Properties/AssemblyInfo.cs
+++ MonoDevelop.FSharp.iOS/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("XamarinStudio.FSharp.iOS")]
+[assembly: AssemblyTitle("MonoDevelop.FSharp.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xamarin Inc.")]
