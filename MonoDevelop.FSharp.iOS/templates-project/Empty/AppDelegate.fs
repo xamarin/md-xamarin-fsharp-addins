d061a7bc83e070c00d57bc2b4402838c73abf76b
Jeffrey Stedfast <jeff@xamarin.com>
2013-03-21 15:26:38 -0400

    [iOS] Updated the AppDelegate.fs template for Empty projects

--- MonoDevelop.FSharp.iOS/templates-project/Empty/AppDelegate.fs
+++ MonoDevelop.FSharp.iOS/templates-project/Empty/AppDelegate.fs
@@ -1,17 +1,35 @@
 namespace ${Namespace}
 
-    open System
-    open MonoTouch.Foundation
-    open MonoTouch.UIKit
+open System
+open MonoTouch.Foundation
+open MonoTouch.UIKit
 
-    [<Register ("AppDelegate")>]
-    type AppDelegate () =
-        inherit UIApplicationDelegate ()
+// The UIApplicationDelegate for the application. This class is responsible for launching the 
+// User Interface of the application, as well as listening (and optionally responding) to 
+// application events from iOS.
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
 
-        [<DefaultValue>]
-        val mutable window : UIWindow
+    //let mutable viewController = Unchecked.defaultof<_>
+    let mutable window = Unchecked.defaultof<_>
 
-        override x.FinishedLaunching (app, options) =
-            let window = new UIWindow (UIScreen.MainScreen.Bounds)
-            window.MakeKeyAndVisible (x)
-            true
+    //
+    // This method is invoked when the application has loaded and is ready to run. In this 
+    // method you should instantiate the window, load the UI into it and then make the window
+    // visible.
+    //
+    // You have 17 seconds to return from this method, or iOS will terminate your application.
+    //
+    override x.FinishedLaunching (app: UIApplication, options: NSDictionary) =
+        // create a new window instance based on the screen size
+        window <- new UIWindow (UIScreen.MainScreen.Bounds)
+
+        // If you have defined a root view controller, set it here:
+        //viewController <- new MyViewController ()
+        //window.RootViewController <- viewController
+
+        // make the window visible
+        window.MakeKeyAndVisible ()
+        true
+
