1cf8326aa3ae0c4fde4ac13bbfbb4a1a6d9dc537
David Siegel <djsiegel@gmail.com>
2013-03-28 10:12:03 -0700

    Ready to run implies loaded.

--- MonoDevelop.FSharp.iOS/templates-project/Empty/AppDelegate.fs
+++ MonoDevelop.FSharp.iOS/templates-project/Empty/AppDelegate.fs
@@ -10,6 +10,7 @@ type AppDelegate () =
 
     let window = new UIWindow (UIScreen.MainScreen.Bounds)
 
+    // This method is invoked when the application is ready to run.
     override this.FinishedLaunching (app, options) =
         // If you have defined a root view controller, set it here:
         // window.RootViewController <- new MyViewController ()
--- MonoDevelop.FSharp.iOS/templates-project/SingleView/Common/AppDelegate.fs
+++ MonoDevelop.FSharp.iOS/templates-project/SingleView/Common/AppDelegate.fs
@@ -10,7 +10,7 @@ type AppDelegate () =
 
     let window = new UIWindow (UIScreen.MainScreen.Bounds)
 
-    // This method is invoked when the application has loaded and is ready to run.
+    // This method is invoked when the application is ready to run.
     override this.FinishedLaunching (app, options) =
         window.RootViewController <- new ${SafeProjectName}ViewController ()
         window.MakeKeyAndVisible ()
